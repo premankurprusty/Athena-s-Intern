3987e827e7b1732160f4acfc87ce057a8b0e7bb2
Premankur Prusty <premankur.prusty@gmail.com>
2026-08-18 17:06:37 +0530

    added comments

--- task2/Premankur_2026AAPS0296H.cpp
+++ task2/Premankur_2026AAPS0296H.cpp
@@ -1,5 +1,6 @@
 #include <Adafruit_LiquidCrystal.h>
 
+// creating an enum of all possible states
 enum class state {
 	opensea,
  	anchored,
@@ -8,6 +9,7 @@ enum class state {
   	wrecked
 };
 
+//declaring the function to update the LCD and setting up initial conditions and variables.
 void updateLCD(state current_state);
 state current_state = state::opensea;
 Adafruit_LiquidCrystal lcd(0);
@@ -28,6 +30,7 @@ unsigned long timer;
 bool anchorDropped = false;
 int lastButtonState = HIGH;
 
+// defining LCD updating logic
 void updateLCD(state current_state) {
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -58,8 +61,12 @@ void updateLCD(state current_state) {
     }
 }
 
+// main loop
 void loop() {
+	// reading brightness level
     int brightness = analogRead(A0);
+
+	// reading distance data
 	digitalWrite(1, LOW);
 	delayMicroseconds(2);
     digitalWrite(1, HIGH);
@@ -67,25 +74,32 @@ void loop() {
     digitalWrite(1, LOW);
     long duration = pulseIn(2, HIGH);
     float distance = duration*0.0343/2.0;
+
+	// reading button
     int buttonState = digitalRead(0);
 
-
+	// checking if button is pressed and chnging anchor state
     if (lastButtonState == HIGH && buttonState == LOW) {
         anchorDropped = !anchorDropped;
     }
 
     lastButtonState = buttonState;
 
+	// handling all the possible states (937 for brightness was from observation)
     switch (current_state) {
         case state::opensea:
             if (anchorDropped) {
                 current_state = state::anchored;
                 updateLCD(current_state);
-            } else if (brightness < 937) {
+            } 
+			
+			else if (brightness < 937) {
                 current_state = state::storm;
                 timer = millis();
                 updateLCD(current_state);
-            } else if (distance < 100) {
+            } 
+			
+			else if (distance < 100) {
                 current_state = state::chardybis;
                 timer = millis();
                 updateLCD(current_state);
@@ -98,11 +112,15 @@ void loop() {
                     current_state = state::storm;
                     timer = millis();
                     updateLCD(current_state);
-                } else if (distance < 100) {
+                } 
+				
+				else if (distance < 100) {
                     current_state = state::chardybis;
                     timer = millis();
                     updateLCD(current_state);
-                } else {
+                } 
+				
+				else {
                     current_state = state::opensea;
                     updateLCD(current_state);
                 }
@@ -113,10 +131,14 @@ void loop() {
             if (anchorDropped) {
                 current_state = state::anchored;
                 updateLCD(current_state);
-            } else if (brightness >= 937) {
+            } 
+			
+			else if (brightness >= 937) {
                 current_state = state::opensea;
                 updateLCD(current_state);
-            } else if (millis() - timer >= 5000) {
+            } 
+			
+			else if (millis() - timer >= 5000) {
                 current_state = state::wrecked;
                 updateLCD(current_state);
             }
@@ -126,16 +148,21 @@ void loop() {
             if (anchorDropped) {
                 current_state = state::anchored;
                 updateLCD(current_state);
-            } else if (distance >= 100) {
+            } 
+			
+			else if (distance >= 100) {
                 current_state = state::opensea;
                 updateLCD(current_state);
-            } else if (millis() - timer >= 5000) {
+            } 
+			
+			else if (millis() - timer >= 5000) {
                 current_state = state::wrecked;
                 updateLCD(current_state);
             }
             break;
     }
 
+	// handling buzzer and LED blinking cases
     if (current_state == state::storm) {
         digitalWrite(12, (millis() / 250) % 2);
     } else {
